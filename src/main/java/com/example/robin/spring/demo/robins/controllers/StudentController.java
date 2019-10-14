e6a92fe7d376f5253b6ae40c10b3265228967997
Robin <swesson92@gmail.com>
2019-10-14 13:47:34 +0200

    controller class added

--- src/main/java/com/example/robin/spring/demo/robins/controllers/StudentController.java
+++ src/main/java/com/example/robin/spring/demo/robins/controllers/StudentController.java
@@ -1,10 +1,28 @@
 package com.example.robin.spring.demo.robins.controllers;
 
+import com.example.robin.spring.demo.robins.models.StudentModel;
+import com.example.robin.spring.demo.robins.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<List<StudentModel>>getAllStudents() {
+
+        List<StudentModel> students = studentService.findAllStudents();
+
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
 }
